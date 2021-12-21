a12f63e92147809f032eddb2a7f93cf3095441ad
Cosmin <ghirisancosmin@gmail.com>
2021-12-21 23:56:50 +0200

    few changes

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 //Base URL
 const token = 'ry749XTxEzbKVGLCSbLEFW2XQeofu0jH3SzRBd5OKUOgIzxuXRY';
-export const championsURL = (page) => `lol/champions?page[number]=${page}&page[size]=10&token=${token}`;
+export const championsURL = (page) => `https://api.pandascore.co/lol/champions?page[number]=${page}&page[size]=10&token=${token}`;
 
